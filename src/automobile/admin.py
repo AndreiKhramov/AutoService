fbc89a374016c4949185eda63a9cbc4eec82db05
Andrey Khramov <95291811+AndreiKhramov@users.noreply.github.com>
2026-08-08 22:57:12 +0300

    Revert "created all models, compose, without sripts file"

--- src/automobile/admin.py
+++
@@ -1,8 +0,0 @@
-from automobile.models.automobile import Automobile
-from automobile.models.autowork import Autowork
-from automobile.models.sparepart import Sparepart
-from django.contrib import admin
-
-admin.site.register(Automobile)
-admin.site.register(Autowork)
-admin.site.register(Sparepart)
--- src/automobile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AutomobileConfig(AppConfig):
-    name = 'automobile'
